b6000d1533580f100890410a35c2af891daf845a
marcobusacca <marcobusacca01@gmail.com>
2023-11-14 12:58:18 +0100

    first commit

---
+++ src/org/lessons/java/shop/Carrello.java
@@ -0,0 +1,11 @@
+package org.lessons.java.shop;
+
+public class Carrello {
+
+	public static void main(String[] args) {
+		
+		Prodotto p = new Prodotto("prodotto", "descrizione", 100, 22);
+		
+		System.out.println(p);
+	}
+}
